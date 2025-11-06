1ba5fcd4eaeda137143596bd7c5d874630524fe5
Tutu <112805308+futu2@users.noreply.github.com>
2025-11-06 17:47:08 +0800

    Enhance build script with additional echo statements

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 wasm_file_path="$(wasm32-wasi-cabal list-bin .)"
 lib_path="$(wasm32-wasi-ghc --print-libdir)" 
+echo "Built Wasm located in: $wasm_file_path"
+echo "GHC lib location: $lib_path"
 wasm32-wasi-cabal build;
+echo "cabal build done"
+ls -alht $wasm_file_path
 bun run "$lib_path/post-link.mjs" -i "$wasm_file_path" -o ghc_wasm_jsffi.js;
+ls -alht ghc_wasm_jsffi.js
 cp "$wasm_file_path" ./final.wasm
 bun build --outdir dist index.html
 echo "build done"
